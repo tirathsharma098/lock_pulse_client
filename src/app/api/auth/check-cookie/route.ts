6ecf1bca8871c12770f6d25ac1a1d877eb4d3fb2
tirathsharma098 <tirathsharma098@gmail.com>
2026-01-28 05:56:04 +0530

    error handling

--- src/app/api/auth/check-cookie/route.ts
+++ src/app/api/auth/check-cookie/route.ts
@@ -2,27 +2,39 @@ import {cookies} from 'next/headers';
 import {NextResponse} from 'next/server';
 
 export async function GET () {
-    const cookie = await cookies();
-    const token = cookie.get("auth");
-    if (token)
+    try{
+        const cookie = await cookies();
+        const token = cookie.get("auth");
+        if (token)
+            return NextResponse.json({
+                success: true
+            }, {status: 200})
+        else return NextResponse.json({
+            success: false
+        }, {status: 401})
+    } catch (err) {
         return NextResponse.json({
-            success: true
-        }, {status: 200})
-    else return NextResponse.json({
-        success: false
-    }, {status: 401})
+            success: false
+        }, {status: 500})
+    }
 }
 
 export async function POST () {
-    const cookie = await cookies();
-    cookie.delete({
-        name: 'auth',
-        path: '/',
-        domain: process.env.COOKIE_DOMAIN,
-        sameSite: 'strict',
-        secure: true,
-    });
-    return NextResponse.json({
-        success: true
-    }, {status: 200});
+    try{
+        const cookie = await cookies();
+        cookie.delete({
+            name: 'auth',
+            path: '/',
+            domain: process.env.COOKIE_DOMAIN,
+            sameSite: 'strict',
+            secure: true,
+        });
+        return NextResponse.json({
+            success: true
+        }, {status: 200});
+    } catch (err){
+        return NextResponse.json({
+            success: false
+        }, {status: 500});
+    }
 }
